dcc3447b7852c4a3d5d64bb74a2fd48b82b34f1b
Freundschaft <Freundschaft>
2022-07-11 01:31:54 +0700

    Fix discord unable to see meta tag.

--- src/components/Layout/AuthWrapper.tsx
+++ src/components/Layout/AuthWrapper.tsx
@@ -6,11 +6,12 @@ const AuthWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
     if (status === "loading") {
         return (
-            <div className="flex w-full h-screen justify-center items-center">
-                <h1 className="text-xl">
-                    Loading ...
-                </h1>
-            </div>
+            <>
+                <div className="flex justify-center items-center absolute top-0 left-0 w-full h-full bg-[#333] z-50">
+                    <h1 className="text-xl">Loading ...</h1>
+                </div>
+                {children}
+            </>
         );
     }
 
