593719f5b536d75a25f1f7ff46e75d8c2b636e1f
Freundschaft <phungtanphat23@gmail.com>
2022-07-31 15:53:08 +0700

    Add gradient background, drop shadow, use ref for password input, update layout.

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,6 +1,7 @@
 import { ActionType, State } from "@/pages";
 import React, { useState } from "react";
-import { FaEye, FaEyeSlash, FaFile, FaLock } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaFile, FaLock, FaUpload } from "react-icons/fa";
+import SpinningCircle from "./SpinningCircle";
 
 const UploadForm: React.FC<{
     state: State;
@@ -10,13 +11,14 @@ const UploadForm: React.FC<{
 
     return (
         <form
-            className="flex flex-col gap-y-6 items-start relative max-w-max p-10 rounded-2xl bg-slate-800 h-[300px]"
+            className="flex flex-col gap-y-6 items-start relative max-w-max p-10 rounded-2xl bg-gradient-to-tl from-slate-800 to-slate-900 h-[300px] drop-shadow-lg"
             onSubmit={(e) => {
                 e.preventDefault();
                 dispatch({ type: "SUBMIT" });
             }}
         >
-            <span className="flex justify-center w-full text-2xl">
+            <span className="flex items-center justify-center w-full gap-2 text-2xl">
+                <FaUpload />
                 Upload File
             </span>
             <label
@@ -24,7 +26,7 @@ const UploadForm: React.FC<{
                 className="flex items-center justify-center gap-2"
             >
                 <FaFile />
-                File:{" "}
+                File:
                 <span className="px-4 py-2 bg-slate-700 rounded-2xl">
                     {state.file?.name || "None"}
                 </span>
@@ -37,42 +39,46 @@ const UploadForm: React.FC<{
                 onChange={(e) =>
                     dispatch({ type: "CHANGE", payload: e.target.files![0] })
                 }
-                required
             />
             <div className="flex items-center justify-center max-w-max gap-x-4">
                 <label
                     htmlFor="password"
                     className="flex items-center justify-center gap-2"
                 >
-                    <FaLock /> Password:{" "}
+                    <FaLock /> Password:
                 </label>
 
-                <input
-                    className="px-4 py-2 bg-slate-700 rounded-2xl focus:outline-none"
-                    type={showPassword ? "password" : "text"}
-                    name="password"
-                    id="password"
-                    onChange={(e) =>
-                        dispatch({ type: "PASSWORD", payload: e.target.value })
-                    }
-                />
-                <button
-                    className="p-3 bg-slate-700 rounded-xl"
-                    aria-label="toggle password display"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setShowPassword((current) => !current);
-                    }}
-                >
-                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </button>
+                <div className="flex items-center justify-center bg-slate-700 rounded-2xl">
+                    <input
+                        className="py-2 ml-4 bg-inherit focus:outline-none"
+                        type={showPassword ? "password" : "text"}
+                        name="password"
+                        id="password"
+                        ref={state.password}
+                    />
+                    <button
+                        className="px-4"
+                        aria-label="toggle password display"
+                        type="button"
+                        onClick={() => setShowPassword((current) => !current)}
+                    >
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </button>
+                </div>
             </div>
 
-            <input
-                className="bg-slate-700 py-2 w-full rounded-2xl h-[40px] cursor-pointer"
+            <button
+                className="bg-slate-700 py-2 w-full rounded-2xl h-[40px] drop-shadow-lg"
                 type="submit"
-                value={state.isUploading ? "Uploading..." : "Submit"}
-            />
+            >
+                {!state.isUploading ? (
+                    "Submit"
+                ) : (
+                    <div className="flex items-center justify-center w-full">
+                        <SpinningCircle />
+                    </div>
+                )}
+            </button>
 
             {state.error && (
                 <div className="absolute right-10 top-[108px]">
