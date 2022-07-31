2f4997fc82b736940dae68bc527fa6f5216ed0ee
Freundschaft <phungtanphat23@gmail.com>
2022-07-31 15:59:42 +0700

    Download with correct file name instead of the path.

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,8 +1,12 @@
-const download = (fileUrl: string, fileName: string) => {
-    let a = document.createElement("a");
-    a.href = fileUrl;
-    a.click();
-    a.remove();
+const download = (fileName: string, fileUrl: string) => {
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.target = "_blank";
+    link.rel = "noreferrer";
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 };
 
 export default download;
