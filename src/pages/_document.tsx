e491f46271509f2bca9b0fe9a0fe4a2ac8a2920e
Freundschaft <phungtanphat23@gmail.com>
2022-08-07 15:45:53 +0700

    Using tailwinc instead.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body>
+            <body className="text-white bg-gradient-to-br from-[#5b6367] to-[#323240]">
                 <Main />
                 <NextScript />
             </body>
