720b3437c94d0a872e2b5f0f025b0c3ba20cf92b
Asakuri Noki <phungtanphat23@gmail.com>
2022-09-04 18:47:03 +0700

    Add user trpc route.

--- src/lib/server/trpc/routers/index.ts
+++ src/lib/server/trpc/routers/index.ts
@@ -4,8 +4,14 @@ import { t } from "../trpc";
 import { checkRouter } from "./check";
 import { fileRouter } from "./file";
 import { uploadRouter } from "./upload";
+import { userRouter } from "./user";
 
-export const appRouter = t.router({ check: checkRouter, file: fileRouter, upload: uploadRouter });
+export const appRouter = t.router({
+    check: checkRouter,
+    file: fileRouter,
+    upload: uploadRouter,
+    user: userRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
