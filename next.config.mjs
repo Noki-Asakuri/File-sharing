78138827f28052cdddce3ad7120b36c7ecb89925
Asakuri Noki <phungtanphat23@gmail.com>
2022-09-13 09:13:16 +0700

    Update deps, remove unuse config in next config.

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,6 @@ function defineNextConfig(config) {
 }
 
 export default defineNextConfig({
-    reactStrictMode: true,
     swcMinify: true,
     images: { domains: ["cdn.discordapp.com"] },
-    experimental: { images: { allowFutureImage: true } },
 });
